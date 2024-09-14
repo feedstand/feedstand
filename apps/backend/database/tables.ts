4c2f4b53791a55921e0070888aa29ff7b7aff2f9
Maciej Lamberski <maciej@lamberski.com>
2024-09-14 21:36:31 +0700

    Use 'number' as bigint representation in the code

--- apps/backend/database/tables.ts
+++ apps/backend/database/tables.ts
@@ -13,7 +13,7 @@ import {
 export const users = pgTable(
     'users',
     {
-        id: bigserial('id', { mode: 'bigint' }).primaryKey(),
+        id: bigserial('id', { mode: 'number' }).primaryKey(),
         name: varchar('name').notNull(),
         email: varchar('email').notNull(),
         password: varchar('password').notNull(),
@@ -29,7 +29,7 @@ export const users = pgTable(
 export const channels = pgTable(
     'channels',
     {
-        id: bigserial('id', { mode: 'bigint' }).primaryKey(),
+        id: bigserial('id', { mode: 'number' }).primaryKey(),
         url: text('url').notNull(),
         title: varchar('title').notNull(),
         link: text('link').notNull(),
@@ -43,8 +43,8 @@ export const channels = pgTable(
 export const items = pgTable(
     'items',
     {
-        id: bigserial('id', { mode: 'bigint' }).primaryKey(),
-        channelId: bigint('channel_id', { mode: 'bigint' })
+        id: bigserial('id', { mode: 'number' }).primaryKey(),
+        channelId: bigint('channel_id', { mode: 'number' })
             .notNull()
             .references(() => channels.id, { onDelete: 'cascade' }),
         title: varchar('title').notNull(),
@@ -69,11 +69,11 @@ export const items = pgTable(
 export const sources = pgTable(
     'sources',
     {
-        id: bigserial('id', { mode: 'bigint' }).primaryKey(),
-        userId: bigint('user_id', { mode: 'bigint' })
+        id: bigserial('id', { mode: 'number' }).primaryKey(),
+        userId: bigint('user_id', { mode: 'number' })
             .notNull()
             .references(() => users.id, { onDelete: 'cascade' }),
-        channelId: bigint('channel_id', { mode: 'bigint' })
+        channelId: bigint('channel_id', { mode: 'number' })
             .notNull()
             .references(() => channels.id, { onDelete: 'cascade' }),
         name: varchar('name').notNull(),
@@ -96,11 +96,11 @@ export const sources = pgTable(
 export const unreads = pgTable(
     'unreads',
     {
-        id: bigserial('id', { mode: 'bigint' }).primaryKey(),
-        userId: bigint('user_id', { mode: 'bigint' })
+        id: bigserial('id', { mode: 'number' }).primaryKey(),
+        userId: bigint('user_id', { mode: 'number' })
             .notNull()
             .references(() => users.id, { onDelete: 'cascade' }),
-        itemId: bigint('item_id', { mode: 'bigint' })
+        itemId: bigint('item_id', { mode: 'number' })
             .notNull()
             .references(() => items.id, { onDelete: 'cascade' }),
     },
