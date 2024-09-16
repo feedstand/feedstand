4e9e9ac8bedf5af0297e10cfbb28e2601d9d00de
Maciej Lamberski <maciej@lamberski.com>
2024-09-16 09:36:58 +0700

    Create helper for validating request structure

---
+++ apps/backend/helpers/routes.ts
@@ -0,0 +1,24 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { ZodError, ZodSchema, z } from 'zod'
+
+export const parseRequestToSchema = <S extends ZodSchema = never>({
+    request,
+    reply,
+    schema,
+    showErrors,
+}: {
+    request: FastifyRequest
+    reply: FastifyReply
+    schema: S
+    showErrors?: boolean
+}): z.infer<S> => {
+    try {
+        return schema.parse(request)
+    } catch (error) {
+        if (error instanceof ZodError) {
+            reply.status(405).send(showErrors ? error.errors : undefined)
+        } else {
+            reply.status(500)
+        }
+    }
+}
