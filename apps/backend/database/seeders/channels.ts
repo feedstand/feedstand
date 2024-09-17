e8ed25d63ff102594c5ef7ef87db809e1ddb2a9e
Maciej Lamberski <maciej@lamberski.com>
2024-09-17 09:45:18 +0700

    Update seeding of Channels by adding new feeds

--- apps/backend/database/seeders/channels.ts
+++ apps/backend/database/seeders/channels.ts
@@ -5,22 +5,25 @@ import { channels } from '~/database/tables.js'
 export const seedChannels = async () => {
     await db.insert(channels).values([
         generateChannel({
-            url: 'https://zenhabits.net/feed/',
+            url: 'https://zenhabits.net/feed',
             title: 'zen habits',
             link: 'https://zenhabits.net',
             description: 'breathe',
-        }),
-        generateChannel({
-            url: 'https://www.thedailyzen.org/feed/',
-            title: 'DAILY ZEN',
-            link: 'https://www.thedailyzen.org',
-            description: undefined,
+            lastScannedAt: undefined,
         }),
         generateChannel({
             url: 'http://deconstructingyourself.com/feed',
             title: 'Deconstructing Yourself',
             link: 'http://deconstructingyourself.com',
             description: 'Mindfulness, Meditation, and Awakening for Modern Mutants',
+            lastScannedAt: undefined,
+        }),
+        generateChannel({
+            url: 'https://daringfireball.net/feeds/json',
+            title: 'Daring Fireball',
+            link: 'https://daringfireball.net',
+            description: undefined,
+            lastScannedAt: undefined,
         }),
     ])
 }
