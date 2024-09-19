1df35bfd3ff8cbc7b0b3334fbdb8a90e40c9d48e
Maciej Lamberski <maciej@lamberski.com>
2024-09-19 20:05:11 +0700

    Extract logic of scanning Channel to an action

--- apps/backend/actions/scanExistingChannel.ts
+++ apps/backend/actions/scanExistingChannel.ts
@@ -1,9 +1,25 @@
 import { eq } from 'drizzle-orm'
-import { channels } from '~/database/tables.js'
+import { channels, items } from '~/database/tables.js'
 import { db } from '~/instances/database.js'
+import { Channel, NewItem } from '~/types/database.js'
+import { fetchAndParseFeed } from './fetchAndParseFeed.js'
 
-export const scanExistingChannel = async (channelId: number) => {
-    console.log(`Perform scanning of Channel #${channelId}`)
+export const scanExistingChannel = async (channel: Channel) => {
+    const feed = await fetchAndParseFeed(channel.url)
 
-    await db.update(channels).set({ lastScannedAt: new Date() }).where(eq(channels.id, channelId))
+    const newItems: Array<NewItem> = []
+
+    for (const newItem of feed.items) {
+        newItems.push({ ...newItem, channelId: channel.id })
+    }
+
+    await db
+        .insert(items)
+        .values(newItems)
+        .onConflictDoNothing({ target: [items.channelId, items.guid] })
+
+    await db
+        .update(channels)
+        .set({ ...feed.channel, lastScannedAt: new Date() })
+        .where(eq(channels.id, channel.id))
 }
--- apps/backend/routes/channels.ts
+++ apps/backend/routes/channels.ts
@@ -1,11 +1,7 @@
 import { app } from '~/instances/server.js'
-import { db } from '~/instances/database.js'
-import { channels, items } from '~/database/tables.js'
-import { eq } from 'drizzle-orm'
-import { NewItem } from '~/types/database.js'
-import { fetchAndParseFeed } from '~/actions/fetchAndParseFeed.js'
 import { fetchChannelById } from '~/actions/fetchChannelById.js'
 import { deleteOrphanChannels } from '~/actions/deleteOrphanChannels.js'
+import { scanExistingChannel } from '~/actions/scanExistingChannel.js'
 
 app.post('/channels/delete', async (request, reply) => {
     // TODO: Move this to background job running periodically.
@@ -28,23 +24,8 @@ app.get('/channels/:id/scan', async (request, reply) => {
     // being added to the feed. Elegant solution: https://stackoverflow.com/a/6651638.
 
     const channel = await fetchChannelById(request)
-    const feed = await fetchAndParseFeed(channel.url)
 
-    const newItems: Array<NewItem> = []
-
-    for (const newItem of feed.items) {
-        newItems.push({ ...newItem, channelId: channel.id })
-    }
-
-    await db
-        .insert(items)
-        .values(newItems)
-        .onConflictDoNothing({ target: [items.channelId, items.guid] })
-
-    await db
-        .update(channels)
-        .set({ ...feed.channel, lastScannedAt: new Date() })
-        .where(eq(channels.id, channel.id))
+    await scanExistingChannel(channel)
 
     return reply.send()
 })
