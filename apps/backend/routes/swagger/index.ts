1ffd061a50936ccbb9359fcd70f08e758d42bc8b
Maciej Lamberski <maciej@lamberski.com>
2024-09-27 17:29:52 +0700

    Replace swagger.json with openapi.json

--- apps/backend/routes/swagger/index.ts
+++ apps/backend/routes/swagger/index.ts
@@ -2,9 +2,9 @@ import { swaggerUI } from '@hono/swagger-ui'
 import { version } from '../../constants/app'
 import { hono } from '../../instances/hono'
 
-hono.get('/swagger', swaggerUI({ url: '/swagger.json' }))
+hono.get('/swagger', swaggerUI({ url: '/openapi.json' }))
 
-hono.doc31('/swagger.json', {
+hono.doc31('/openapi.json', {
     openapi: '3.1.0',
     info: {
         title: 'Feedstand API',
