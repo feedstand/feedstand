6ff82cae1cfcec17f5487c2a7e142d9e014cd8f5
Maciej Lamberski <maciej@lamberski.com>
2024-09-30 13:59:47 +0700

    Use ?? instead of ||

--- apps/backend/constants/database.ts
+++ apps/backend/constants/database.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
-export const host = process.env.DATABASE_HOST || ''
+export const host = process.env.DATABASE_HOST ?? ''
 export const port = process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : undefined
-export const database = process.env.DATABASE_NAME || ''
+export const database = process.env.DATABASE_NAME ?? ''
 export const user = process.env.DATABASE_USER
 export const password = process.env.DATABASE_PASS
