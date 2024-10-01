28eb99381bc34f13a45a81e990b6f24c56e7c8fd
Maciej Lamberski <maciej@lamberski.com>
2024-10-01 21:18:13 +0700

    Finalize frontend Docker setup for prod

--- apps/frontend/.docker/entrypoint.sh
+++ apps/frontend/.docker/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-ALLOWED_VARS=$(grep -v '^#' .env.production | cut -d= -f1 | sed 's/^/$/' | tr '\n' ' ')
+ALLOWED_VARS=$(grep -v '^#' .env.production | cut -d= -f1 | sed 's/^VITE_//' | sed 's/^/$/' | tr '\n' ' ')
 
 for file in /usr/share/nginx/html/assets/*.js; do
     envsubst "$ALLOWED_VARS" < "$file" > "${file}.tmp"
     mv "${file}.tmp" "$file"
 done
+
+exec "$@"
