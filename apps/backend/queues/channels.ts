cd08efa7771a8ea71a1b101aa1a268acf95cb617
Maciej Lamberski <maciej@lamberski.com>
2024-10-02 14:25:28 +0700

    Fix not triggering background jobs

--- apps/backend/queues/channels.ts
+++ apps/backend/queues/channels.ts
@@ -9,15 +9,15 @@ export const channelsQueue = createQueue('channels', {
 })
 
 const repeatableJobOptions: JobsOptions = {
-    repeat: { pattern: '*/15 * * * *', limit: 0 },
+    repeat: { pattern: '*/15 * * * *' },
     removeOnComplete: false,
     removeOnFail: false,
 }
 
-setInterval(
-    () => {
-        channelsQueue.add('deleteOrphanChannels', undefined, repeatableJobOptions)
-        channelsQueue.add('scanChannels', undefined, repeatableJobOptions)
-    },
-    5 * 60 * 1000,
-)
+const addRepeatableJobs = () => {
+    channelsQueue.add('deleteOrphanChannels', undefined, repeatableJobOptions)
+    channelsQueue.add('scanChannels', undefined, repeatableJobOptions)
+}
+
+setInterval(addRepeatableJobs, 5 * 60 * 1000)
+addRepeatableJobs()
