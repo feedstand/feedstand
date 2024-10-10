251e577341439847884d02fd9d9137e09e26f088
Maciej Lamberski <maciej@lamberski.com>
2024-10-10 11:22:41 +0700

    Add option to find feed when passing the feed URL directly

--- apps/backend/actions/fetchAndFindFeeds.ts
+++ apps/backend/actions/fetchAndFindFeeds.ts
@@ -1,6 +1,6 @@
 import { HTTPException } from 'hono/http-exception'
 import { JSDOM } from 'jsdom'
-import { feedLinkSelectors, htmlContentTypes } from '../constants/scrapers'
+import { anyFeedContentTypes, feedLinkSelectors, htmlContentTypes } from '../constants/scrapers'
 import { isOneOfContentTypes } from '../helpers/scrapers'
 import { FeedInfo } from '../types/schemas'
 import { fetchAndParseFeed } from './fetchAndParseFeed'
@@ -8,23 +8,29 @@ import { fetchAndParseFeed } from './fetchAndParseFeed'
 // TODO: Make it extensible by providing a way to parse and discover feeds differently depending on
 // the service. For example, if a user provides a link to a YouTube video, we could obtain the HTML
 // of the author's YouTube channel and retrieve the RSS feed from there.
-export const fetchAndFindFeeds = async (pageUrl: string): Promise<Array<FeedInfo>> => {
+export const fetchAndFindFeeds = async (pageOrFeedUrl: string): Promise<Array<FeedInfo>> => {
     // TODO: Enable caching of requests based on headers in the response.
-    const response = await fetch(pageUrl)
+    const response = await fetch(pageOrFeedUrl)
+
+    if (isOneOfContentTypes(response, anyFeedContentTypes)) {
+        const { channel } = await fetchAndParseFeed(pageOrFeedUrl)
+
+        return [{ title: channel.title, url: channel.url }]
+    }
 
     if (!isOneOfContentTypes(response, htmlContentTypes)) {
         throw new HTTPException(422)
     }
 
     const html = await response.text()
-    const jsdom = new JSDOM(html, { url: pageUrl })
+    const jsdom = new JSDOM(html, { url: pageOrFeedUrl })
     const feedLinks = jsdom.window.document.querySelectorAll(feedLinkSelectors.join())
     const feedInfos: Array<FeedInfo> = []
 
     for (const feedLink of feedLinks) {
         const linkHref = feedLink.getAttribute('href')
         const feedTitle = feedLink.getAttribute('title')
-        const feedUrl = linkHref ? new URL(linkHref, pageUrl).href : undefined
+        const feedUrl = linkHref ? new URL(linkHref, pageOrFeedUrl).href : undefined
 
         if (!feedUrl) {
             continue
--- apps/backend/constants/scrapers.ts
+++ apps/backend/constants/scrapers.ts
@@ -15,3 +15,5 @@ export const xmlFeedContentTypes = [
     'application/xml',
     'text/xml',
 ]
+
+export const anyFeedContentTypes = [...jsonFeedContentTypes, ...xmlFeedContentTypes]
