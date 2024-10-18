48b2e1c92dac044df2762ecf16a7556feb0dbe17
Maciej Lamberski <maciej@lamberski.com>
2024-10-18 20:13:43 +0700

    Add user-agent header when fetching external URLs

--- apps/backend/actions/fetchExternalUrl.ts
+++ apps/backend/actions/fetchExternalUrl.ts
@@ -1,4 +1,4 @@
 export const fetchExternalUrl = async (url: string): Promise<Response> => {
     // TODO: Enable caching of requests based on headers in the response.
-    return await fetch(url)
+    return await fetch(url, { headers: { 'User-Agent': 'PHP' } })
 }
