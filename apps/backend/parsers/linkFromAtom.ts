67707dee8159ff723b53bd1aec6a922fd607cc3f
Maciej Lamberski <maciej@lamberski.com>
2024-10-18 20:24:26 +0700

    Add comment regarding the use case of this parser

--- apps/backend/parsers/linkFromAtom.ts
+++ apps/backend/parsers/linkFromAtom.ts
@@ -1,5 +1,16 @@
 import { get } from 'lodash-es'
 
+// Example value:
+// {
+//   value: [Object: null prototype] {
+//     '$': [Object: null prototype] {
+//       rel: 'self',
+//       href: 'https://www.inc.com/rss/',
+//       xmlns: 'http://www.w3.org/2005/Atom'
+//     }
+//   }
+// }
+// Example feed: https://www.inc.com/rss/.
 export const linkFromAtom = (value: unknown) => {
     if (get(value, '$.xmlns') === 'http://www.w3.org/2005/Atom') {
         return get(value, '$.href')
