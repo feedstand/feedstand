e679440fe08c733466a1e3883ad6598204f1552f
Maciej Lamberski <maciej@lamberski.com>
2024-10-21 12:25:24 +0700

    Update comment

--- apps/backend/parsers/dateMidnightTime.ts
+++ apps/backend/parsers/dateMidnightTime.ts
@@ -1,7 +1,7 @@
 import { dayjs } from '../instances/dayjs'
 
 // Replace 24 with 00 in feeds that contain incorrect hour notation in the date.
-// Example value: Tue, 15 Oct 2024 24:15:01 +0530.
+// Example: Tue, 15 Oct 2024 24:15:01 +0530.
 // Example feed: https://www.news18.com/commonfeeds/v1/eng/rss/world.xml.
 export const dateMidnightTime = (value: unknown) => {
     if (typeof value !== 'string') {
