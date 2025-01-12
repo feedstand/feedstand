a30428b9ff1e8ad61b932467db1e50d5c07f4107
Maciej Lamberski <maciej@lamberski.com>
2025-01-12 17:41:35 +0100

    Bump version of packages in the frontend app

--- apps/frontend/apis/channels.ts
+++ apps/frontend/apis/channels.ts
@@ -1,7 +1,7 @@
 import { openapi } from '../instances/openapi'
 
 export const loadChannel = async (channelId: number) => {
-    const request = await openapi.channels[':id'].$get({ param: { id: channelId.toString() } })
+    const request = await openapi.channels[':id'].$get({ param: { id: channelId } })
     const response = await request.json()
 
     return response
