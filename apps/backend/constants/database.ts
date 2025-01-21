8e7dfe51960fc2fc82cf3dcf44535623e7f8b045
Maciej Lamberski <maciej@lamberski.com>
2025-01-21 12:12:38 +0100

    Revert any changes related to Dokploy

--- apps/backend/constants/database.ts
+++ apps/backend/constants/database.ts
@@ -1,4 +1,4 @@
-// import 'dotenv/config'
+import 'dotenv/config'
 
 export const host = process.env.DATABASE_HOST ?? ''
 export const port = process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : undefined
--- apps/backend/constants/server.ts
+++ apps/backend/constants/server.ts
@@ -1,4 +1,4 @@
-// import 'dotenv/config'
+import 'dotenv/config'
 
 export const host = process.env.SERVER_HOST ?? ''
 export const port = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : undefined
