c2efb4cbf7435f8d96b770cdc522d38e60573981
Maciej Lamberski <maciej@lamberski.com>
2025-01-21 16:58:00 +0100

    Set user/pass for the queue service

--- apps/backend/constants/queue.ts
+++ apps/backend/constants/queue.ts
@@ -2,3 +2,5 @@ import 'dotenv/config'
 
 export const host = process.env.QUEUE_HOST ?? ''
 export const port = process.env.QUEUE_PORT ? Number(process.env.QUEUE_PORT) : undefined
+export const user = process.env.QUEUE_USER ?? ''
+export const pass = process.env.QUEUE_PASS ?? ''
--- apps/backend/instances/queue.ts
+++ apps/backend/instances/queue.ts
@@ -4,5 +4,7 @@ import * as queueConstants from '../constants/queue'
 export const connection = new Redis({
     host: queueConstants.host,
     port: queueConstants.port,
+    username: queueConstants.user,
+    password: queueConstants.pass,
     maxRetriesPerRequest: null,
 })
