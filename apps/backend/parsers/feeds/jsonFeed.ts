fc87117e8fc5a2f253d8fbf6689a5d9e1ab73e1a
Maciej Lamberski <maciej@lamberski.com>
2025-01-27 20:43:05 +0100

    Add parsing of values inside JSON feeds

--- apps/backend/parsers/feeds/jsonFeed.ts
+++ apps/backend/parsers/feeds/jsonFeed.ts
@@ -1,18 +1,22 @@
 import { castArray, get } from 'lodash-es'
 import { jsonFeedContentTypes } from '../../constants/parsers'
-import { trimStrings } from '../../helpers/parsers'
+import { parseValue, trimStrings } from '../../helpers/parsers'
 import { isOneOfContentTypes } from '../../helpers/responses'
 import { JsonFeed } from '../../types/feeds'
 import { FeedChannel, FeedItem } from '../../types/schemas'
 import { FeedParser } from '../../types/system'
+import { dateAi } from '../values/dateAi'
+import { dateCustomFormat } from '../values/dateCustomFormat'
+import { dateStandard } from '../values/dateStandard'
+import { textStandard } from '../values/textStandard'
 
 export const jsonFeedChannel = (feed: JsonFeed, url: string): FeedChannel => {
-    return {
-        url: feed.feed_url || url,
-        title: feed.title,
-        link: feed.home_page_url,
-        description: feed.description,
-    }
+    return trimStrings({
+        url: parseValue(feed.feed_url, [textStandard], url),
+        title: parseValue(feed.title, [textStandard]),
+        link: parseValue(feed.home_page_url, [textStandard]),
+        description: parseValue(feed.description, [textStandard]),
+    })
 }
 
 export const jsonFeedItems = (feed: JsonFeed): Array<FeedItem> => {
@@ -27,15 +31,21 @@ export const jsonFeedItems = (feed: JsonFeed): Array<FeedItem> => {
             continue
         }
 
+        const parsedLink = parseValue(item.url, [textStandard], '')
+
         items.push(
             trimStrings({
-                title: item.title,
-                link: item.url,
-                description: item.summary,
-                author: get(item.authors, '0.name'),
-                guid: item.id || item.url,
-                content: item.content_html || item.content_text,
-                publishedAt: item.date_published,
+                link: parsedLink,
+                guid: parseValue(item.id, [textStandard], parsedLink),
+                title: parseValue(item.title, [textStandard]),
+                description: parseValue(item.summary, [textStandard]),
+                author: parseValue(get(item.authors, '0.name'), [textStandard]),
+                content: parseValue(item.content_html || item.content_text, [textStandard]),
+                publishedAt: parseValue(
+                    item.date_published,
+                    [dateStandard, dateCustomFormat, dateAi],
+                    new Date(),
+                ),
             }),
         )
     }
@@ -43,7 +53,7 @@ export const jsonFeedItems = (feed: JsonFeed): Array<FeedItem> => {
     return items
 }
 
-export const jsonFeed: FeedParser = async (response, options) => {
+export const jsonFeed: FeedParser = async (response) => {
     if (!isOneOfContentTypes(response, jsonFeedContentTypes)) {
         return
     }
