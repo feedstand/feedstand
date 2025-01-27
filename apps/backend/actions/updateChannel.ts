f2c39c7840e5afb40f3094cf42ed05eeb9c3450d
Maciej Lamberski <maciej@lamberski.com>
2025-01-27 15:58:56 +0100

    Remove no longer necessary comment

--- apps/backend/actions/updateChannel.ts
+++ apps/backend/actions/updateChannel.ts
@@ -7,7 +7,6 @@ export const updateChannel = async (channel: Channel, updates: FeedChannel) => {
     await db
         .update(tables.channels)
         .set({
-            // TODO: Use lodash.defaults instead of `??`?
             title: updates.title ?? channel.title,
             description: updates.description ?? channel.description,
             link: updates.link ?? channel.link,
