4b5182ace181a1465e055e3185446473f2d3c288
Maciej Lamberski <maciej@lamberski.com>
2025-01-28 09:34:23 +0100

    Fix issue with inserting too many items to DB when scanning channels

--- apps/backend/actions/createOrUpdateItems.ts
+++ apps/backend/actions/createOrUpdateItems.ts
@@ -2,6 +2,8 @@ import { tables } from '../database/tables'
 import { db } from '../instances/database'
 import { Channel, FeedItem, NewItem } from '../types/schemas'
 
+const ITEMS_CHUNK_SIZE = 5000
+
 export const createOrUpdateItems = async (channel: Channel, items: Array<FeedItem>) => {
     if (items.length === 0) {
         return
@@ -17,8 +19,16 @@ export const createOrUpdateItems = async (channel: Channel, items: Array<FeedIte
     // we always store the newest version of it.
     // Consideration: Should the changes history be kept to highlight changes in the Item?
 
-    await db
-        .insert(tables.items)
-        .values(newItems)
-        .onConflictDoNothing({ target: [tables.items.channelId, tables.items.guid] })
+    // Store items in chunks for cases where the large number of items would cause Postgres error
+    // "MAX_PARAMETERS_EXCEEDED: Max number of parameters (65534) exceeded".
+    // TODO: IF at any point it is considered to only store X amount of recent items for each
+    // channel, this could be updated to cap inserted items.
+    for (let i = 0; i < newItems.length; i += ITEMS_CHUNK_SIZE) {
+        const chunkItems = newItems.slice(i, i + ITEMS_CHUNK_SIZE)
+
+        await db
+            .insert(tables.items)
+            .values(chunkItems)
+            .onConflictDoNothing({ target: [tables.items.channelId, tables.items.guid] })
+    }
 }
