39f682b524495255846af7196e98695656f47a41
Maciej Lamberski <maciej@lamberski.com>
2025-01-31 20:47:20 +0100

    Include drizzle-kit in the production packages

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,7 +1,10 @@
 import { serve } from '@hono/node-server'
+import { fetchOrCreateChannel } from './actions/fetchOrCreateChannel'
+import { isWorker } from './constants/queue'
 import * as serverConstants from './constants/server'
 import { importFilesFromDirectory } from './helpers/files'
 import { hono } from './instances/hono'
+import { scanChannel } from './jobs/scanChannel'
 
 await importFilesFromDirectory('./queues')
 
@@ -10,3 +13,7 @@ serve({
     hostname: serverConstants.host,
     port: serverConstants.port,
 })
+
+if (!isWorker) {
+    scanChannel(await fetchOrCreateChannel('https://journal.jatan.space/rss/'))
+}
--- apps/backend/jobs/scanChannel.ts
+++ apps/backend/jobs/scanChannel.ts
@@ -3,6 +3,7 @@ import { createOrUpdateItems } from '../actions/createOrUpdateItems'
 import { fetchFeed } from '../actions/fetchFeed'
 import { parseFeed } from '../actions/parseFeed'
 import { updateChannel } from '../actions/updateChannel'
+import { isWorker } from '../constants/queue'
 import { tables } from '../database/tables'
 import { convertErrorToString } from '../helpers/errors'
 import { db } from '../instances/database'
@@ -13,9 +14,17 @@ export const scanChannel = async (channel: Channel) => {
         const response = await fetchFeed(channel.url, { channel })
         const feed = await parseFeed(response, { channel })
 
+        if (!isWorker) {
+            console.error({ channel })
+        }
+
         createOrUpdateItems(channel, feed.items)
         updateChannel(channel, feed.channel)
     } catch (error) {
+        if (!isWorker) {
+            console.error({ channel, error })
+        }
+
         // TODO: Store more error details for further debug proces. Things to consider storing:
         // Whole Response object, body, status code, number of errors since last successful scan.
         await db
