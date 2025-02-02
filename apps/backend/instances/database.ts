7c0bff8625f9bf6cf749d7170b03d3806fb658cc
Maciej Lamberski <maciej@lamberski.com>
2025-02-02 16:49:17 +0100

    Improve the connection settings for database

--- apps/backend/instances/database.ts
+++ apps/backend/instances/database.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as databaseConstants from '../constants/database'
+import { hasWorkerFeature } from '../constants/features'
 import * as schema from '../database/tables'
 
 export const client = postgres({
@@ -9,6 +10,10 @@ export const client = postgres({
     database: databaseConstants.database,
     username: databaseConstants.user,
     password: databaseConstants.password,
+    max: hasWorkerFeature ? 5 : 8,
+    idle_timeout: 20,
+    connect_timeout: 10,
+    max_lifetime: 1800,
 })
 
 export const db = drizzle(client, { schema })
