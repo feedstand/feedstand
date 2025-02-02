dcce8b0804a1d27b3da996850c403f0511bfa1dc
Maciej Lamberski <maciej@lamberski.com>
2025-02-02 10:39:42 +0100

    Implement batch scheduling of scanChannel background jobs

--- apps/backend/jobs/scanChannels.ts
+++ apps/backend/jobs/scanChannels.ts
@@ -1,16 +1,36 @@
+import { gt } from 'drizzle-orm'
 import { tables } from '../database/tables'
+import { sleep } from '../helpers/system'
 import { db } from '../instances/database'
 import { channelQueue } from '../queues/channel'
+
+const CHANNELS_CHUNK_SIZE = 5000
+const CHANNELS_CHUNK_DELAY = 5000
 
 export const scanChannels = async () => {
     // TODO: Consider adding support for adjusting scanning frequency based on the actual new items
     // being added to the feed. Elegant solution: https://stackoverflow.com/a/6651638.
-    const channels = await db.select().from(tables.channels)
 
-    // TODO: Brake this into queuing by smaller chunks as fetching 150k records at once is killing
-    // the app.
+    let lastId = 0
 
-    for (const channel of channels) {
-        channelQueue.add('scanChannel', channel)
+    while (true) {
+        const channels = await db
+            .select()
+            .from(tables.channels)
+            .where(gt(tables.channels.id, lastId))
+            .orderBy(tables.channels.id)
+            .limit(CHANNELS_CHUNK_SIZE)
+
+        if (channels.length === 0) {
+            break
+        }
+
+        for (const channel of channels) {
+            await channelQueue.add('scanChannel', channel)
+        }
+
+        lastId = channels[channels.length - 1].id
+
+        await sleep(CHANNELS_CHUNK_DELAY)
     }
 }
