971c247bf6bdcbcdee0a129218b0303fa9899565
Maciej Lamberski <maciej@lamberski.com>
2025-02-07 15:36:43 +0100

    Combine fetchers and parsers constants

--- apps/backend/constants/fetchers.ts
+++ apps/backend/constants/fetchers.ts
@@ -1,1 +1,18 @@
 export const maxTimeout = 20 * 1000
+
+export const htmlContentTypes = ['text/html', 'application/xhtml+xml']
+
+export const jsonFeedContentTypes = ['application/json']
+
+export const xmlFeedContentTypes = [
+    'application/rdf+xml',
+    'application/rss+xml',
+    'application/atom+xml',
+    'application/xml', // Not ideal, it should probably be discarded at some point.
+    'text/rdf+xml', // Legacy, for supporting very old websites only.
+    'text/rss+xml', // Legacy, for supporting very old websites only.
+    'text/atom+xml', // Legacy, for supporting very old websites only.
+    'text/xml', // Legacy, for supporting very old websites only.
+]
+
+export const anyFeedContentTypes = [...jsonFeedContentTypes, ...xmlFeedContentTypes]
