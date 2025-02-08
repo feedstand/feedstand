c6084d01f41541f032ee48f1e1b7336de9d42e2d
Maciej Lamberski <maciej@lamberski.com>
2025-02-08 09:58:06 +0100

    Remove no longer used types

--- apps/backend/types/system.ts
+++ apps/backend/types/system.ts
@@ -1,10 +1,1 @@
-import { Channel, FeedInfo } from './schemas'
-
-export type FeedFinder = (
-    response: Response,
-    options?: {
-        channel?: Channel
-    },
-) => Promise<Array<FeedInfo> | undefined>
-
 export type ValueParser<R, V = unknown> = (value: V) => R | undefined
