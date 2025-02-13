c15b4834f222859d151bd8f2213f83e7deb6ce47
Maciej Lamberski <maciej@lamberski.com>
2025-02-13 07:33:53 +0100

    Remove memory debugging code

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,7 +1,7 @@
 import { serve } from '@hono/node-server'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import * as databaseConstants from './constants/database'
-import { hasMigratorFeature, hasServerFeature, hasWorkerFeature } from './constants/features'
+import { hasMigratorFeature, hasServerFeature } from './constants/features'
 import * as serverConstants from './constants/server'
 import { importFilesFromDirectory } from './helpers/files'
 import { db } from './instances/database'
@@ -19,21 +19,4 @@ if (hasServerFeature) {
         hostname: serverConstants.host,
         port: serverConstants.port,
     })
-}
-
-if (hasWorkerFeature) {
-    const logMemoryInfo = () => {
-        const used = process.memoryUsage()
-        console.log({
-            rss: Math.round(used.rss / 1024 / 1024),
-            heapTotal: Math.round(used.heapTotal / 1024 / 1024),
-            heapUsed: Math.round(used.heapUsed / 1024 / 1024),
-            external: Math.round(used.external / 1024 / 1024),
-        })
-    }
-
-    process.on('SIGTERM', logMemoryInfo)
-    process.on('SIGINT', logMemoryInfo)
-
-    setInterval(logMemoryInfo, 10000)
 }
