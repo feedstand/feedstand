0897665ac6bad13d579e867bb302b4f409970cc0
Maciej Lamberski <maciej@lamberski.com>
2025-02-14 18:12:10 +0100

    Remove temporary Redis settings

--- apps/backend/instances/queue.ts
+++ apps/backend/instances/queue.ts
@@ -7,6 +7,4 @@ export const connection = new Redis({
     username: queueConstants.user,
     password: queueConstants.pass,
     maxRetriesPerRequest: null,
-    enableOfflineQueue: false, // Important settings for bulk operations.
-    enableAutoPipelining: true, // Pipeline commands instead of sending them one by one.
 })
