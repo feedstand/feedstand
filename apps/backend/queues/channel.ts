a64260d3ed8dc1dd431fe408426a2ccc09fb7f08
Maciej Lamberski <maciej@lamberski.com>
2025-02-14 18:16:44 +0100

    Do an experiment of running 10 parallel jobs per worker instance

--- apps/backend/queues/channel.ts
+++ apps/backend/queues/channel.ts
@@ -2,4 +2,8 @@ import { createQueue } from '../helpers/queues'
 import { fixChannel } from '../jobs/fixChannel'
 import { scanChannel } from '../jobs/scanChannel'
 
-export const channelQueue = createQueue('channel', { scanChannel, fixChannel })
+export const channelQueue = createQueue(
+    'channel',
+    { scanChannel, fixChannel },
+    { worker: { concurrency: 10 } },
+)
