4ce35d2ebf3e5c397eac2103cfc9bc283d6c6033
Maciej Lamberski <maciej@lamberski.com>
2025-02-24 21:48:43 +0100

    Improve robustness of the logic for detecting existing Channels

--- apps/backend/actions/fetchOrCreateChannel.ts
+++ apps/backend/actions/fetchOrCreateChannel.ts
@@ -4,19 +4,37 @@ import { tables } from '../database/tables'
 import { db } from '../instances/database'
 import { Channel } from '../types/schemas'
 import { createOrUpdateItems } from './createOrUpdateItems'
+import { fetchUrl } from './fetchUrl'
 
-export const fetchOrCreateChannel = async (url: string): Promise<Channel> => {
+const fetchExistingChannel = async (feedUrl: string) => {
   const [existingChannel] = await db
     .select()
     .from(tables.channels)
-    .where(eq(tables.channels.feedUrl, url))
+    .where(eq(tables.channels.feedUrl, feedUrl))
     .limit(1)
+
+  return existingChannel
+}
+
+export const fetchOrCreateChannel = async (url: string): Promise<Channel> => {
+  // First, check if a channel already exists in the database with the provided URL as is.
+  // This is an optimization to avoid unnecessary network requests if the URL is already known.
+  let feedUrl = url
+  let existingChannel = await fetchExistingChannel(feedUrl)
+
+  // If no channel was found with the initial URL, it's possible the provided URL is an old feed
+  // URL that has been redirected to a new, canonical URL.
+  if (!existingChannel) {
+    feedUrl = (await fetchUrl(feedUrl, { method: 'head' })).url
+    existingChannel = await fetchExistingChannel(feedUrl)
+  }
 
   if (existingChannel) {
     return existingChannel
   }
 
-  const feedData = await fetchFeed({ url })
+  const feedData = await fetchFeed({ url: feedUrl })
+
   const [newChannel] = await db
     .insert(tables.channels)
     .values({
