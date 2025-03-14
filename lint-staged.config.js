2cf34ba83d8802bb9ef6d8341bd028aa1ca918bb
Maciej Lamberski <maciej@lamberski.com>
2025-03-14 19:34:41 +0100

    Replace Husky & lint-staged with Lefthook

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  '*.{ts,tsx}': [() => 'npm run lint:tsc', 'npm run lint:prettier:base'],
-  '*.{js,jsx}': ['npm run lint:prettier:base'],
-  '*.{yml,yaml}': ['npm run lint:prettier:base'],
-  '*.json': ['npm run lint:prettier:base'],
-}
