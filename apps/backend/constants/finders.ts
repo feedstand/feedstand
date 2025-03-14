94ac1fb0917025fcf488fc881d4005153125b779
Maciej Lamberski <maciej@lamberski.com>
2025-03-14 19:36:59 +0100

    Add list of potential paths where feeds might be located

--- apps/backend/constants/finders.ts
+++ apps/backend/constants/finders.ts
@@ -5,3 +5,32 @@ export const feedLinkSelectors = anyFeedContentTypes.map(
 )
 
 export const youTubeDomains = ['youtu.be', 'youtube.com']
+
+export const feedUris = [
+  '/feed',
+  '/rss',
+  '/atom',
+  '/rss.xml',
+  '/atom.xml',
+  '/feed.xml',
+  '/feed.rss',
+  '/feed.atom',
+  '/feed.rss.xml',
+  '/feed.atom.xml',
+  '/index.xml',
+  '/index.rss',
+  '/index.atom',
+  '/index.rss.xml',
+  '/index.atom.xml',
+  '/?format=rss',
+  '/?format=atom',
+  '/?rss=1',
+  '/?atom=1',
+  '/?feed=rss',
+  '/?feed=rss2',
+  '/?feed=atom',
+  '/.rss',
+  '/f.json',
+  '/feed.json',
+  '/json',
+]
