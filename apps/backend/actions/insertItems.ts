0fd9ac834f8bec03960d4f62c731e88489446fc2
Maciej Lamberski <maciej@lamberski.com>
2025-03-28 16:14:23 +0100

    Update comment

--- apps/backend/actions/insertItems.ts
+++ apps/backend/actions/insertItems.ts
@@ -15,8 +15,9 @@ export const insertItems = async (channel: Channel, items: Array<FeedItem>) => {
     newItems.push({ ...newItem, channelId: channel.id })
   }
 
-  // TODO: IF at any point it is considered to only store X amount of recent items for each
-  // channel, this could be updated to cap inserted items.
+  // TODO: If at some point it is considered to only store X number of recent items for each
+  // channel, this can be updated to cap inserted items AND removing the existing ones exceeding
+  // the X number of recent items.
   for (let i = 0; i < newItems.length; i += ITEMS_CHUNK_SIZE) {
     const chunkItems = newItems.slice(i, i + ITEMS_CHUNK_SIZE)
 
