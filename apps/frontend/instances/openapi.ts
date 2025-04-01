ea3d0633421bc3feedeea8ac75d12c8bfb118901
Maciej Lamberski <maciej@lamberski.com>
2025-04-01 20:45:14 +0200

    Apply Biome fixes

--- apps/frontend/instances/openapi.ts
+++ apps/frontend/instances/openapi.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@feedstand/backend/types/openapi'
+import type { Routes } from '@feedstand/backend/types/openapi'
 import { hc } from 'hono/client'
 import { url } from '../constants/backend'
 
