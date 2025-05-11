1400be54e09f7329ab7ffa2c5f7806342169d47b
Maciej Lamberski <maciej@lamberski.com>
2025-05-11 14:42:09 +0200

    Remove unnecessary helper

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -3,11 +3,8 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import * as databaseConstants from './constants/database.ts'
 import { hasMigratorFeature, hasServerFeature } from './constants/features.ts'
 import * as serverConstants from './constants/server.ts'
-import { importFilesFromDirectory } from './helpers/files.ts'
 import { db } from './instances/database.ts'
 import { hono } from './instances/hono.ts'
-
-await importFilesFromDirectory('./queues')
 
 if (hasMigratorFeature) {
   await migrate(db, databaseConstants)
