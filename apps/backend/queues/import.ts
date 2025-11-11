0eaefda3abd08f897384a5a559237559a5d1c3e7
Maciej Lamberski <maciej@lamberski.com>
2025-11-11 11:44:21 +0100

    Increase worker concurrency

--- apps/backend/queues/import.ts
+++ apps/backend/queues/import.ts
@@ -18,7 +18,7 @@ export const importQueue = createQueue(
       },
     },
     worker: {
-      concurrency: 7,
+      concurrency: 10,
       lockDuration: 60000,
       maxStalledCount: 5,
       stalledInterval: 60000,
