31e98bf2eb65617ed516264b874bfab0b1111fa6
Maciej Lamberski <maciej@lamberski.com>
2025-11-14 18:36:40 +0100

    Assert url as string after it's checked by refine()

--- apps/backend/schemas/safeUrl.ts
+++ apps/backend/schemas/safeUrl.ts
@@ -5,3 +5,4 @@ export const safeUrl = z
   .url()
   .transform((url) => prepareUrl(url, { validate: true }))
   .refine((url) => url, { message: 'Invalid URL' })
+  .transform((url) => url as string)
